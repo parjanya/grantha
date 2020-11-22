fb2e5e204a0c38bc9a1c39849534bd2062b38545
Edgard <bikelis@gmail.com>
2020-11-22 11:04:29 -0300

    cosmetic changes

--- grantha-dependent.lisp
+++ grantha-dependent.lisp
@@ -4,12 +4,12 @@
 
   Author: Edgard Bikelis (bikelis@gmail.com)
 
-  Carteiro is free software: you can redistribute it and/or modify
+  Grantha is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or (at
   your option) any later version.
 
-  Carteiro is distributed in the hope that it will be useful,
+  Grantha is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
